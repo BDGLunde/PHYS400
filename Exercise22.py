dcfe29b8736fa9bc2e4b08114f0d3c9608c90f01
Edward <elunde@calpoly.edu>
2012-04-01 15:35:00 -0700

    Exercise 2.2

---
+++ Exercise22.py
@@ -0,0 +1,13 @@
+#Exercise 2.2
+
+5
+x = 5
+x + 1
+
+# Without the print statement, this script does not return an output when run.
+
+print 5
+x = 5
+print x + 1
+
+# To display the result of a statement in a script, we must utilize print.
