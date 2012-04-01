9bc34a94835d1cf24b6d925451b21fca40bbaac0
Edward <elunde@calpoly.edu>
2012-04-01 15:35:55 -0700

    Exercise 3.3

---
+++ Exercise33.py
@@ -0,0 +1,12 @@
+
+#provides an indentation of 70 spaces and prints the desired string
+
+def right_justify(s):
+    print " "*70 + s
+
+
+right_justify('test') #for testing through use in cmd.
+
+    
+    
+
