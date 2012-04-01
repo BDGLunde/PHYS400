22435aef37f023766d1e9cf9970dc5946b92ee49
Edward <elunde@calpoly.edu>
2012-04-01 15:35:28 -0700

    Exercise 3.1

---
+++ Exercise31.py
@@ -0,0 +1,19 @@
+#Exercise 3.1
+
+repeat_lyrics()
+
+def print_lyrics():
+    print "I cut down trees. I eat my lunch."
+    print "I go to the lavatory."
+
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+#error message returned is:
+    #File "Exercise31.py", line 3, in <module>
+    #       repeat_lyrics()
+    #NameError: name 'repeat_lyrics' is not defined
+
+    
