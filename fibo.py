e4007dabee979d31a6fdb2494adb649d7f242bde
Edward <elunde@calpoly.edu>
2012-04-01 15:37:28 -0700

    Added credit to python for fibo code to comments

---
+++ fibo.py
@@ -0,0 +1,21 @@
+# Fibonacci numbers module
+
+def fib(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    while b < n:
+        print b,
+        a, b = b, a+b
+
+def fib2(n): # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while b < n:
+        result.append(b)
+        a, b = b, a+b
+    return result
+
+
+print 'Test'
+
+#Script taken from http://docs.python.org/tutorial/modules.html.
+#Used this as an example to play around with scripts.
