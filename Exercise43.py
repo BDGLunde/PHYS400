10ceb3016e7f508028209eecb61e6ea090684923
Edward <elunde@calpoly.edu>
2012-04-14 02:07:42 -0700

    Trimmed down Exercise 4.3

--- Exercise43.py
+++ Exercise43.py
@@ -3,17 +3,6 @@ from math import *
 from Exercise41 import *
 
 
-def triangle(t,length):
-    lt(t,30)
-    for i in range(6):
-        
-        fd(t,length)
-        lt(t,180-60)
-        fd(t,length)
-        lt(t,180-60)
-        fd(t,length)
-        lt(t,180)
-   
 def tri(t,length,angle):
     b=length*sin(angle/2*pi/180)
     rt(t,angle/2)
@@ -31,16 +20,11 @@ def tripie(n,t,length):
         lt(t,angle)
             
 
-
-
-
 world = TurtleWorld()
 bob = Turtle()
 bob.delay = 0.1
 
-tripie(50,bob,100)
+tripie(6,bob,100)
 
     
 
-
-wait_for_user()
