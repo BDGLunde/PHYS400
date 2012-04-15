6e6cec5568744594d23fc530ab94e1f524cf1121
Edward <elunde@calpoly.edu>
2012-04-15 16:03:04 -0700

    Exercise 6.3: is_between(x,y,z)

---
+++ Exercise63.py
@@ -0,0 +1,11 @@
+#Exercise 6.3 - is_between(x,y,z)
+
+from math import *
+
+
+def is_between(x,y,z):
+    """Returns True if x<=y<=z or False otherwise."""
+
+    return x<=y<=z
+
+print is_between(5,5,5)
