7ba0c9419fc8b3b41e57e9491ff1f92205501ff5
Edward <elunde@calpoly.edu>
2012-04-15 14:26:34 -0700

    Exercise 6.1: Comparing two numbers

---
+++ Exercise61.py
@@ -0,0 +1,16 @@
+#Exercise 6.1
+
+from math import *
+
+def compare(x,y):
+    """Takes floats x and y and compares their values. x>y returns 1
+    x=y returns 0 and x<y returns -1.
+    """
+    if (x>y):
+        return 1
+    if(x==y):
+        return 0
+    else:
+        return -1
+    
+
